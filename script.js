94777eb2b10d4ff4ba2dd9248c5f0087ea744f5a
R. LaRoi <are@raewhoo.com>
2024-06-10 16:10:50 -0400

    CSS

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 
 document.getElementById("start-btn").addEventListener("click", (e) => {
   e.preventDefault("");
-  let userName = prompt("what is you name?").toUpperCase();
+  let userName = prompt("What is your name?").toUpperCase();
 
   // create element <h1>
   let greeting = (document.createElement(
     "h1"
-  ).innerHTML = `<div>WELCOME ${userName}!</div>`);
+  ).innerHTML = `<div>Welcome ${userName}!</div>`);
   console.log(greeting);
 
   // select using querySelector
@@ -18,8 +18,16 @@ document.getElementById("start-btn").addEventListener("click", (e) => {
   subText.innerHTML = greeting;
 
   let readBtn = document.querySelector("button");
-  readBtn.textContent = "Let's read a book!";
-  readBtn.disabled = true;
+  readBtn.style.display = "none";
+
+  // append to parent element
+  let imgDiv = document.createElement("div");
+  let nextArrow = document.createElement("img");
+  nextArrow.classList.add("arrow-gif");
+  nextArrow.src =
+    "https://github.com/R-LaRoi/sba-v3/assets/114012059/71db82a0-9cf7-46b8-9055-896e714e3307";
+
+  imgDiv.appendChild(nextArrow);
 });
 
 // Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
