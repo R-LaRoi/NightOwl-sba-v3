82a159b97b996f4a54dfef66e2dc212f5f5e44f4
R. LaRoi <are@raewhoo.com>
2024-06-10 13:07:11 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// sba.v3  ---------
+
+// selectElementbyID
+
+document.getElementById("start-btn").addEventListener("click", (e) => {
+  e.preventDefault("");
+  let userName = prompt("what is you name?").toUpperCase();
+  console.log(userName);
+
+  // create element <h1>
+  let greeting = (document.createElement(
+    "h1"
+  ).innerHTML = `<div>WELCOME ${userName}!</div>`);
+  console.log(greeting);
+
+  // select using querySelector
+  let subText = document.querySelector("h3");
+  subText.innerHTML = greeting;
+
+  let readBtn = document.querySelector("button");
+
+  readBtn.textContent = "Let's read a book!";
+  console.log(readBtn);
+});
+
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+
+// // Use appendChild and/or prepend to add new elements to the DOM.
+
+// selector or selector All
+
+// Create at least one element using createElement.
+
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
