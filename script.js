076a6e9833502eaf477cf6d791108664e659ca6f
R. LaRoi <are@raewhoo.com>
2024-06-10 21:05:13 -0400

    Nav

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // sba.v3  ---------
 
+let navData = [
+  { id: 1, title: "MBC" },
+  { id: 3, title: "Explore" },
+  { id: 3, title: "Book List" },
+];
+
+console.log(navData);
+
 // selectElementbyID
+document.getElementById("section-two").style.display = "none";
 
 document.getElementById("start-btn").addEventListener("click", (e) => {
   e.preventDefault("");
@@ -8,8 +17,11 @@ document.getElementById("start-btn").addEventListener("click", (e) => {
 
   // create element <h1>
   let greeting = (document.createElement(
-    "h1"
-  ).innerHTML = `<div>Welcome ${userName}!</div>`);
+    "div"
+  ).innerHTML = `<h1>Welcome ${userName}!</h1><p>
+Let's check out some new books!
+  </p>
+`);
   console.log(greeting);
 
   // select using querySelector
@@ -21,13 +33,44 @@ document.getElementById("start-btn").addEventListener("click", (e) => {
   readBtn.style.display = "none";
 
   // append to parent element
-  let imgDiv = document.createElement("div");
+
   let nextArrow = document.createElement("img");
   nextArrow.classList.add("arrow-gif");
   nextArrow.src =
     "https://github.com/R-LaRoi/sba-v3/assets/114012059/71db82a0-9cf7-46b8-9055-896e714e3307";
 
-  imgDiv.appendChild(nextArrow);
+  document.getElementById("arrow").appendChild(nextArrow);
+});
+
+let showNextScreen = document.getElementById("arrow");
+showNextScreen.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let screenOne = document.getElementById("section-one");
+  screenOne.remove();
+
+  // -----screen two---------
+  document.getElementById("section-two").style.display = "block";
+
+  // nav styles
+  let navBar = document.getElementById("nav");
+  navBar.style.background = "#121716";
+  navBar.style.height = "75px";
+  navBar.style.boxShadow =
+    "box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;";
+  navBar.style.display = "flex";
+  navBar.style.gap = "20px";
+
+  navData.map((link) => {
+    // 2. create an a element
+    // 3. add href
+    let ulink = document.createElement("li");
+    ulink.innerHTML = `${link.title}`;
+    console.log(ulink);
+
+    // 5. append element
+    navBar.append(ulink);
+  });
 });
 
 // Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
